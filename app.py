4280fa438c596524deda89a55bbe10cd18b486ac
maso0310 <balimon@hotmail.com>
2018-09-26 17:31:52 +0800

    'ok'

--- app.py
+++ app.py
@@ -40,9 +40,7 @@ def callback():
 def handle_message(event):
     if event.message.text == '我想買' :
         print(event.message.text)
-        a=event.message.text - '我想買'
-        print(a)
-        url = 'https://tw.shop.com/maso0310/search/'+ a
+        url = 'https://tw.shop.com/maso0310/search/'+ event.message.text -'我想買'
         print(url)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=url))
 
