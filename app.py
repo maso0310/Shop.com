ebc1c5243893c379f01b07bac6223a2d21075460
maso0310 <balimon@hotmail.com>
2018-09-27 11:26:52 +0800

    'ok'

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, abort
 import requests
-import urllib2
+import urllib3
 import random
 from bs4 import BeautifulSoup
 from linebot import (
@@ -36,19 +36,19 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    url = 'https://tw.shop.com/maso0310/search'+event.message.text
     my_headers=["Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0""Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)"]
-    
-
+    url = 'https://tw.shop.com/maso0310/search'+event.message.text
     randdom_header=random.choice(my_headers)
-    req = urllib2.Request(url)
+
     req.add_header("User-Agent",randdom_header)
     req.add_header("Host","e-hentai.org")
-    req.add_header("Referer","https://e-hentai.org/lofi")
-    req.add_header("GET",url)
+    req.add_header("Referer","https://tw.shop.com")
 
+    resp = requests.get(url , headers = randdom_header)
+    print(resp.headers)
     print(resp.text)
     print(resp.status_code)
+
     if resp and state_code == 200:
         soup = BeautifulSoup(resp.text, 'html.parser')
         product_info = soup.find_all('div','product-results__prod-info-m')
