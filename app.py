fdd0e44918fcf8ae921ae04c3e66340dc3699227
maso0310 <balimon@hotmail.com>
2018-10-18 21:40:22 +0800

    'ok'

--- app.py
+++ app.py
@@ -36,13 +36,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
     url = 'https://tw.shop.com/maso0310/search'+event.message.text
-    UserAgent = ["Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:30.0) Gecko/20100101 Firefox/30.0","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14","Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Win64; x64; Trident/6.0)"]
-    randdom_header = random.choice(UserAgent)
-    print(randdom_header)
-
-    resp = requests.get(url)
+    resp = requests.get(url,proxies={'http':'http://27.120.89.131:8080'})
     print(resp)
     print(resp.status_code)
 
